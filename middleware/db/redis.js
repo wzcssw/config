b42d19a3a7fd54396ab3abe55d66d37c3553d5bc
wangsen <wangs@tongxinyiliao.com>
2016-04-14 17:16:38 +0800

    .

--- middleware/db/redis.js
+++ middleware/db/redis.js
@@ -4,12 +4,6 @@ rdb = redis.createClient();
 
 // 选择数据库，比如第5个数据库，默认是第0个
 rdb.select(5);
-rdb.set('test_key', 'test_value1');
-rdb.expire('test_key', 5);
-rdb.get('test_key', function(err, value){
-    if (err) throw(err);
-    console.log('test_key:' + value);
-});
 
 //错误监听
 rdb.on("error", function (err) {
