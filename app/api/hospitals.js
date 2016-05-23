3b53fb40f376940bbf3520ab6900a1e6a15123d1
王森 <wangs@tongxinyiliao.com>
2016-05-23 14:43:05 +0800

    remove JSON.parse stringify

--- app/api/hospitals.js
+++ app/api/hospitals.js
@@ -6,20 +6,19 @@ var router = require('koa-router')({
 router.get('/', function*() {
     "use strict";
     var self = this;
-    var hospitals;
     var page = 1;
     var params = self.query;
     if (params.page != null) {
         page = params.page;
     }
+    this.logger.info(page);
     var result = yield http.get('/v1/config_hospital/list', {
         fields: 'id,name,pinyin,level,city_id,province_id',
         page: page
     });
-    hospitals = JSON.stringify(result.hospitals);
     self.body = {
         success: true,
-        hospitals: hospitals,
+        hospitals: result.hospitals,
         total_count: result.total_count,
         current_page: result.current_page
     }
--- app/public/scripts/controllers.js
+++ app/public/scripts/controllers.js
@@ -1,64 +1,64 @@
 var controllers = angular.module('controllers', ['services', 'directives']);
-controllers.controller('loginController', ['$scope', 'userHttp', '$state', function($scope, userHttp, $state){
-	$scope.isShow = false;
-	$scope.self = $scope;
-	userHttp.getUser(function(user){
-		"use strict";
-		if (user){
-			$state.go('main');
-		}else {
-			$scope.isShow = true;
-		}
-	});
+controllers.controller('loginController', ['$scope', 'userHttp', '$state', function ($scope, userHttp, $state) {
+    $scope.isShow = false;
+    $scope.self = $scope;
+    userHttp.getUser(function (user) {
+        "use strict";
+        if (user) {
+            $state.go('main');
+        } else {
+            $scope.isShow = true;
+        }
+    });
 
-	$scope.login = function(){
-		"use strict";
-		userHttp.login({username: $scope.username, password: $scope.password}, function (data) {
-			userHttp.user = data.user;
-			location.href = '/';
-		});
-	};
+    $scope.login = function () {
+        "use strict";
+        userHttp.login({username: $scope.username, password: $scope.password}, function (data) {
+            userHttp.user = data.user;
+            location.href = '/';
+        });
+    };
 }]);
 
-controllers.controller('mainController', ['$scope', 'userHttp', function($scope, userHttp){
-	$scope.self = $scope;
-    $scope.register = function(){
-		"use strict";
-		userHttp.register({username: $scope.username, password: $scope.password}, function (data) {
-			var result = data.result;
-			console.log(data);
-		});
-	}
+controllers.controller('mainController', ['$scope', 'userHttp', function ($scope, userHttp) {
+    $scope.self = $scope;
+    $scope.register = function () {
+        "use strict";
+        userHttp.register({username: $scope.username, password: $scope.password}, function (data) {
+            var result = data.result;
+            console.log(data);
+        });
+    }
 }]);
 
-controllers.controller('hospitalsController', ['$scope', 'hospitalHttp', '$state', function($scope, hospitalHttp, $state){
-	"use strict";
-	$scope.self = $scope;
-	$scope.maxSize = 5;
-	hospitalHttp.getHospital({},function(data){
-	  $scope.hospitals = JSON.parse(data.hospitals);
-	  $scope.current_page = data.current_page;
-	  $scope.total_count = data.total_count; 
-	  console.log($scope.total_count);
-	});
-	$scope.pageChanged = function(){
-		hospitalHttp.getHospital({page: $scope.current_page},function(data){
-		  $scope.current_page = data.current_page;	  
-      $scope.hospitals = JSON.parse(data.hospitals);
-		});
-	};
+controllers.controller('hospitalsController', ['$scope', 'hospitalHttp', '$state', function ($scope, hospitalHttp, $state) {
+    "use strict";
+    $scope.self = $scope;
+    $scope.maxSize = 5;
+    hospitalHttp.getHospital({}, function (data) {
+        $scope.hospitals = data.hospitals;
+        $scope.current_page = data.current_page;
+        $scope.total_count = data.total_count;
+        console.log($scope.total_count);
+    });
+    $scope.pageChanged = function () {
+        hospitalHttp.getHospital({page: $scope.current_page}, function (data) {
+            $scope.current_page = data.current_page;
+            $scope.hospitals = data.hospitals;
+        });
+    };
 
-  $scope.setPage = function () {
-    $scope.current_page = $('#go_page').val();
-    $scope.pageChanged();
-    $('#go_page').val("");
-  }; 
+    $scope.setPage = function () {
+        $scope.current_page = $('#go_page').val();
+        $scope.pageChanged();
+        $('#go_page').val("");
+    };
 
 }]);
 
-controllers.controller('createHospitalController', ['$scope', '$state', 'userHttp', function($scope, $state, userHttp){
-  $scope.self = $scope;
-  'use strict'; 
+controllers.controller('createHospitalController', ['$scope', '$state', 'userHttp', function ($scope, $state, userHttp) {
+    $scope.self = $scope;
+    'use strict';
 }]);
 
 
