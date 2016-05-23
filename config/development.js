6417200812e7c18a7d5dacb2b6b777b8ec3473f0
王森 <wangs@tongxinyiliao.com>
2016-05-23 13:29:30 +0800

    add field

--- config/development.js
+++ config/development.js
@@ -3,16 +3,16 @@ module.exports = {
     winston: {
         level: 'debug'
     },
-    //api: {
-    //    host: '127.0.0.1',
-    //    port: 3000,
-    //    token: "2BbasqQg75g",
-    //    key: 's0JLjZ'
-    //},
     api: {
-        host: 'apitest.txzs.org',
-        port: 80,
+        host: '127.0.0.1',
+        port: 3000,
         token: "2BbasqQg75g",
         key: 's0JLjZ'
     }
+    //api: {
+    //    host: 'apitest.txzs.org',
+    //    port: 80,
+    //    token: "2BbasqQg75g",
+    //    key: 's0JLjZ'
+    //}
 };
