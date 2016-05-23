9a4d38b5666f1cebc800ac360f83a5b954b8611f
王森 <wangs@tongxinyiliao.com>
2016-05-23 15:07:06 +0800

    add navbar active

--- app/public/scripts/app.js
+++ app/public/scripts/app.js
@@ -1,10 +1,14 @@
 var app = angular.module('myApp', ['ui.bootstrap', 'routes', 'controllers', 'filters']);
-app.run(['$rootScope', 'userHttp', function($rootScope, userHttp){
+app.run(['$rootScope', '$location', 'userHttp', function($rootScope, $location, userHttp){
     $rootScope.logout = function(){
         "use strict";
         userHttp.logout(function(){
             console.log('logout success');
             location.href = '/';
-        })
-    }
+        });
+    };
+
+    $rootScope.isActive = function (viewLocation) {
+        return viewLocation === $location.path();
+    };
 }]);
