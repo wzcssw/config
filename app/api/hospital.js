665fd5cbfff04ae7b076b6ddbe7c2268471e3e56
王森 <wangs@tongxinyiliao.com>
2016-05-23 11:12:05 +0800

    little modify

--- app/api/hospital.js
+++ app/api/hospital.js
@@ -1,29 +1,28 @@
 var common = require('../common');
-var session = common.session;
 var http = common.http;
 var router = require('koa-router')({
     prefix: '/api/hospitals'
 });
-var page = 1;
-router.get('/hospitals', function*(){
-	"use strict";
-	var self = this;
-	var hospitals;
-  var params = self.query;
-  if(params.page!=null){
-    page = params.page;
-  };
-  var result = yield http.get('/v1/config_hospital/list', {
-    fields: 'id,name,pinyin,level,city_id,province_id',
-    page: page
-  });
-  hospitals = JSON.stringify(result.hospitals);
-  self.body = {
-    success: true,
-    hospitals: hospitals,
-    total_count: result.total_count,
-    current_page: result.current_page
-  }
+router.get('/hospitals', function*() {
+    "use strict";
+    var self = this;
+    var hospitals;
+    var page = 1;
+    var params = self.query;
+    if (params.page != null) {
+        page = params.page;
+    }
+    var result = yield http.get('/v1/config_hospital/list', {
+        fields: 'id,name,pinyin,level,city_id,province_id',
+        page: page
+    });
+    hospitals = JSON.stringify(result.hospitals);
+    self.body = {
+        success: true,
+        hospitals: hospitals,
+        total_count: result.total_count,
+        current_page: result.current_page
+    }
 });
 
 module.exports = router.routes();
